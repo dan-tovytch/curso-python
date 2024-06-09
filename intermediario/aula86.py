4eb901e0e55b30b8ae08342012666f84ec931352
dan-tovytch <danielmelentovytch@gmail.com>
2024-06-09 00:00:45 -0300

    aula86

---
+++ intermediario/aula86.py
@@ -0,0 +1,29 @@
+# Dictionary Comprehension e Set Comprehension
+
+produto = {
+    'nome': 'Caneta Azul',
+    'preco': 2.5,
+    'categoria': 'Escritório'
+}
+
+dc = {
+    chave: valor.upper()
+    if isinstance(valor, str) else valor
+    for chave, valor in produto.items()
+    if chave != 'categoria'
+}
+
+lista = {
+    ('a', 'valor a'),
+    ('b', 'valor b'),
+    ('c', 'valor c'),
+}
+
+dc = {
+    chave: valor
+    for chave, valor in lista
+}
+
+s1 = {2 ** i for i in range(10)}
+
+print(s1)
