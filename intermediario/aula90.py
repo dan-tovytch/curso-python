1ae93232ca6581b1020e8aab5999720d9c2d3b85
Dan <danielmelentovytch@gmail.com>
2024-09-03 11:36:34 -0300

    aula90

---
+++ intermediario/aula90.py
@@ -0,0 +1,7 @@
+# Generator expression, Iterables e Iteradores em Python
+interable = ['Eu', 'Tenho', '__iter__']
+iterator = iter(interable) # tem __iter__ e __next__
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
