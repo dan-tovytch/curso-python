9e278f03248ee72148723f4c25a2d029b43c7b79
Dan <danielmelentovytch@gmail.com>
2024-09-03 21:12:22 -0300

    update aula90

--- intermediario/aula90.py
+++ intermediario/aula90.py
@@ -1,7 +1,9 @@
+import sys
+
 # Generator expression, Iterables e Iteradores em Python
 interable = ['Eu', 'Tenho', '__iter__']
 iterator = iter(interable) # tem __iter__ e __next__
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
+lista = [n for n in range(10)] 
+generetor = (n for n in range(10))
+print(sys.getsizeof(lista))
+print(sys.getsizeof(generetor))
